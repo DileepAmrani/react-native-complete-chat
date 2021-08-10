4bed24f8e983891f0c74e4dbf515c24d5377640a
DileepAmrani <amranidileep44@gmail.com>
2021-08-11 02:55:30 +0500

    test version

---
+++ README.md
@@ -0,0 +1,8 @@
+# What is this?
+
+A React Native Chat solution.
+
+# installation
+
+`npm i react-native-complete-chat`
+
---
+++ index.js
@@ -0,0 +1,6 @@
+function CompleteChat(){
+    <View>
+        <Text>React Native Complete Chat</Text>
+    </View>
+}
+module.exports.CompleteChat = CompleteChat
